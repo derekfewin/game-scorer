4785f35efa7ec90d02ff894c18a491f14efda927
Derek Fewin <derek.fewin@gmail.com>
2026-01-21 15:01:04 -0600

    Updated the viewer join v3

--- js/state.js
+++ js/state.js
@@ -13,6 +13,6 @@ let state = {
     viewerCount: 0,
     viewerId: null,          // Unique ID for this browser session
     viewingAsPlayerIdx: null, // The index of the player we are "being" (0, 1, 2...)
-    firebaseRef: null
+    firebaseRef: null,
 	connectedViewers: null
 };
